85b6f735776a7320d561789518f9d28ef16e75ea
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-09 22:07:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,10 @@ hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-st.subheader('Map of all pickups')
-st.map(data)
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
 
 year = st.slider('choose year?', 1800, 2022, 1999)
 st.write("This Map Represents year:  ", year)
