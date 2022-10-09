fb7ddc6a1fe8642c37d0e2e2719b1c8a4221c147
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-09 21:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 @st.cache
 def load_data(nrows):
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    def load_data(nrows): 
+         data = pd.read_csv(DATA_URL, nrows=nrows)
+         lowercase = lambda x: str(x).lower()
+         data.rename(lowercase, axis='columns', inplace=True)
+         data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+         return data
 
 # Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
+    data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+    data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
+    data_load_state.text("Done! (using st.cache)")
 
 year = st.slider('choose year?', 1800, 2022, 1999)
 st.write("This Map Represents year:  ", year)
