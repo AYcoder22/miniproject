882e07ce4abf387cf94b71bfb07ec854857e6a22
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-09 23:06:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-"""
+
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
@@ -34,7 +41,8 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
-"""
+
+
 
 st.title('Map data')
 st.title('part 2')
