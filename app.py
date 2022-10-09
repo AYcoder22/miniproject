5c0cbabc4277a9c3dd0f283b751f7826d9144bdb
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-09 23:24:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,10 +82,3 @@ except URLError as e:
         % e.reason
     )
 
-
-[theme]
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
