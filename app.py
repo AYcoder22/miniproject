b5b74244f3ae7e0c1f5d51f0cadf1ab4a6ac6f24
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-09 21:44:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-
 import streamlit as st
 import pandas as pd
+import numpy as np
 
+st.title('URBANIZATION WEBSITE')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
 year = st.slider('choose year?', 1800, 2022, 1999)
 st.write("This Map Represents year:  ", year)
