c52724a7b318eefbc405dfaa5f6461a624462e42
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-09 21:59:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import numpy as np
 st.title('URBANIZATION WEBSITE')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 st.subheader('Raw data')
 st.write(data)
