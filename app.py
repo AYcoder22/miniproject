f55c8ba217faff035e612692ba8728cedccc22c2
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-09 19:25:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 import pandas as pd
 
 
-year = st.slider('choose year?', 0, 130, 25)
+year = st.slider('choose year?', 1800, 2022, 1999)
 st.write("This Map Represents year:  ", year)
