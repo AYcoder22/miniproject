036545169eb18df5e397fb8f4b9b17345da39dbe
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-10 08:26:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
 
 st.title('URBANIZATION MEASUREMENT')
 
