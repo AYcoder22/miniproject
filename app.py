d8e232ce5ce30b4877f9a46b0ddefe4e3a820eb4
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-10 08:09:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-
-st.title('Uber pickups in NYC')
+st.title('URBANIZATION MEASUREMENT')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -20,7 +19,7 @@ def load_data(nrows):
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+data_load_state.text("Loading data...Done!")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
@@ -31,10 +30,10 @@ hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+year = st.slider('year', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == year]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.subheader('Map of all pickups at %s:00' % year)
 st.map(filtered_data)
 
 
