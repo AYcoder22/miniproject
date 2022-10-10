5d7363e098a1325c69acfa8b30d01c1ce6fdaccd
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-10 08:21:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
+
     selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
     page_names_to_funcs[selected_page]()
 
@@ -63,16 +61,4 @@ def main_page():
             st.altair_chart(chart, use_container_width=True)
 
             
-def page2():
-    st.markdown("# Page 2 ❄️")
-    st.sidebar.markdown("# Page 2 ❄️")
 
-def page3():
-    st.markdown("# Page 3 🎉")
-    st.sidebar.markdown("# Page 3 🎉")
-
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
-}
