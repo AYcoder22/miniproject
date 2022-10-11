1ea1fc8647309efc745a336e0493663b258f2053
Akash <69795479+AYcoder22@users.noreply.github.com>
2022-10-11 16:05:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,5 @@
 
 
-    
-    
-#1
-
-st.set_page_config(
-    page_title="Multipage App",
-    page_icon="👋",
-)
-
-st.title("Main Page")
-st.sidebar.success("Select a page above.")
-
-if "my_input" not in st.session_state:
-    st.session_state["my_input"] = ""
-
-my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("Submit")
-if submit:
-    st.session_state["my_input"] = my_input
-    st.write("You have entered: ", my_input)
-
-
-
-
-
 #2
 import streamlit as st
 import pandas as pd
